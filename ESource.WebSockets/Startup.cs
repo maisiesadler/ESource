890ad4a0a10b64cb9b11b729964deceae73ce3e3
maisiesadler <maisiesadler@hotmail.com>
2018-03-09 06:58:22 +0000

    Add websocket api

---
+++ ESource.WebSockets/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ESource.WebSockets
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            app.UseWebSockets();
+
+            var manager = new WebSocketConnectionManager();
+
+            app.Use(manager.HandleRequest);
+
+            //app.Use(async (context, next) =>
+            //{
+            //    var http = (HttpContext)context;
+
+            //    if (http.WebSockets.IsWebSocketRequest)
+            //    {
+            //        WebSocket webSocket = await http.WebSockets.AcceptWebSocketAsync();
+            //        var bytes = Encoding.UTF8.GetBytes("testing132");
+            //        await webSocket.SendAsync(bytes, WebSocketMessageType.Text, true, _token);
+            //    }
+            //});
+        }
+    }
+}
---
+++ ESource.WebSockets/WebSocketConnectionManager.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ESource.WebSockets
+{
+    public class Model
+    {
+        public CancellationTokenSource CancellationToken { get; }
+        public ReplaySubject<string> Subject { get; }
+        public Model(ReplaySubject<string> subject, CancellationTokenSource cancellationToken)
+        {
+            Subject = subject;
+            CancellationToken = cancellationToken;
+        }
+    }
+    
+    public class WebSocketConnectionManager
+    {
+        Dictionary<string, Model> _models = new Dictionary<string, Model>();
+        CancellationToken _token = new CancellationTokenSource().Token;
+        public WebSocketConnectionManager()
+        {
+        }
+
+        private Model CreateModel(string name)
+        {
+            var model = new ReplaySubject<string>();
+            var cancellationToken = new CancellationTokenSource();
+            Task.Run(async () =>
+             {
+                 var a = 0;
+                 while (!cancellationToken.IsCancellationRequested)
+                 {
+                     await Task.Delay(1000);
+                     model.OnNext(name + a++);
+                 }
+                 model.OnCompleted();
+             }, cancellationToken.Token);
+
+            return new Model(model, cancellationToken);
+        }
+
+        private ReplaySubject<string> GetOrCreateModel(string name)
+        {
+            if (!_models.ContainsKey(name))
+            {
+                _models[name] = CreateModel(name);
+            }
+            return _models[name].Subject;
+        }
+
+        private void TryCancel(string name)
+        {
+            if (_models.ContainsKey(name))
+            {
+                _models[name].CancellationToken.Cancel();
+            }
+        }
+
+        public async Task HandleRequest(HttpContext context, Func<Task> next)
+        {
+            var http = (HttpContext)context;
+
+            if (http.WebSockets.IsWebSocketRequest)
+            {
+                WebSocket webSocket = await http.WebSockets.AcceptWebSocketAsync();
+                var re = await webSocket.ReceiveAsync(new ArraySegment<byte>(), new CancellationToken());
+
+                var isRequestingModel = http.Request.Headers.TryGetValue("modelname", out var vals);
+
+                if (isRequestingModel)
+                {
+                    var tcs = new TaskCompletionSource<object>();
+                    var subject = GetOrCreateModel(vals);
+                    var subscription = subject.Subscribe(async s =>
+                    {
+                        var c = http;
+                        var bytes = Encoding.UTF8.GetBytes(s);
+                        await webSocket.SendAsync(bytes, WebSocketMessageType.Text, true, _token);
+                    }, () => { tcs.SetResult(new object()); });
+                    await Task.Delay(3000);
+                    TryCancel(vals);
+                    await tcs.Task;
+                    subscription.Dispose();
+                }
+            }
+        }
+    }
+}
